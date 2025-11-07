917d08e569268e118f4bfe34e9a7822e29c79f85
Sharwin Patil <sharwin24@gmail.com>
2025-11-07 13:58:45 -0600

    Don't instantiate FrankaPlugin if not available and DockerFile updates

--- potential_fields/src/ros/pfield_manager.cpp
+++ potential_fields/src/ros/pfield_manager.cpp
@@ -94,11 +94,13 @@ PotentialFieldManager::PotentialFieldManager() : Node("potential_field_manager")
   if (this->motionPluginType.empty()) {
     this->motionPlugin = std::make_unique<NullMotionPlugin>();
   }
+  #ifdef USING_FRANKA
   else if (this->motionPluginType == "franka") {
     std::string frankaHostname = this->declare_parameter("franka_hostname", std::string());
     frankaHostname = this->get_parameter("franka_hostname").as_string();
     this->motionPlugin = std::make_unique<FrankaPlugin>(frankaHostname);
   }
+  #endif // USING_FRANKA
   else if (this->motionPluginType == "xarm") {
     this->motionPlugin = std::make_unique<XArmPlugin>();
   }
